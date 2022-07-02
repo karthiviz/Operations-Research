932b5a703b1b86c34e4fcbcb0aa561e464e26c2f
karthiviz <63582886+karthiviz@users.noreply.github.com>
2022-07-02 16:25:23 +0530

    Update scheduling_exercise.py

--- OR(2)/scheduling_exercise.py
+++ OR(2)/scheduling_exercise.py
@@ -13,7 +13,7 @@ jobs = pd.read_excel('exercise.xlsx', 'Jobs', index_col=0)
 jobs.sort_values(by='Processing time', axis=0, ascending=False, inplace=True)
 jobs.reset_index(inplace=True)
 jobs.index = np.arange(1,len(jobs)+1)
-machines = dict(zip(["m"+str(i) for i in range(MACHINES)],[[0,0],[0,1],[0,2]]))
+machines = dict(zip(["m"+str(i) for i in range(MACHINES)], [[0,i] for i in range(MACHINES)]))
 
 for i in jobs.index:
     machines = dict(sorted(machines.items(), key=lambda machine: (machine[1][0], machine[1][1])))
